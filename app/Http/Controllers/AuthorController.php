97ec34fb2ccc4a9347cd62f9137e7d7f68a1843d
Ramon Lima <ramonlima08@gmail.com>
2021-11-30 16:18:19 -0300

    feat: Add controladores e Traits

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    use ApiResponser;
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($author)   
+    {
+        
+    }
+
+    public function update(Request $request, $author)
+    {
+        
+    }
+
+    public function destroy($author)
+    {
+        
+    }
+
+    
+}
---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    use ApiResponser;
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public function index()
+    {
+        
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($book)   
+    {
+        
+    }
+
+    public function update(Request $request, $book)
+    {
+        
+    }
+
+    public function destroy($book)
+    {
+        
+    }
+
+    
+}
