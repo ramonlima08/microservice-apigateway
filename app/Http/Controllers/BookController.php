4103471972191781892db72f4f5843b5edf7804a
Ramon Lima <ramonlima08@gmail.com>
2021-11-30 21:39:42 -0300

    feat: Add Tratamento de erros no Exception Handler - Add validação de Author na criação de livros

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\AuthorService;
 use App\Services\BookService;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
@@ -12,15 +13,17 @@ class BookController extends Controller
     use ApiResponser;
 
     public $bookService;
+    public $authorService;
 
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(BookService $bookService)
+    public function __construct(BookService $bookService, AuthorService $authorService)
     {
         $this->bookService = $bookService;
+        $this->authorService = $authorService;
     }
 
     public function index()
@@ -29,6 +32,9 @@ class BookController extends Controller
     }
 
     public function store(Request $request){
+
+        $this->authorService->obtainAuthor($request->author_id);
+
         return $this->successResponse($this->bookService->createBooks($request->all()), Response::HTTP_CREATED);
     }
 
